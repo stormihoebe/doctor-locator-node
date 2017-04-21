5ef0398ada90e475b8f5c15003cc114e8b69dfcb
Stormi Hoebelheinrich <Stormi Hoebelheinrich>
2017-04-21 11:00:23 -0700

    add jquery to collect input from forms

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   // Call Geo Complete
   $("#address").geocomplete({details:"form#property"});
-  $("")
+  $("#property").submit(function(event){
+    event.preventDefault();
+    var lat = $("#lat").val();
+    var long = $("#long").val();
+    var range = $("#range").val();
+    var issue = $("#issue").val();
+    console.log("lat--" + lat);
+    console.log("long--" + long);
+    console.log("range--" + range);
+    console.log("issue--" + issue);
+  });
 });
